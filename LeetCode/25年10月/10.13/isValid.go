3c52a2e638f584caf9cf37cae19207e2e44a13ff
AK12_Official <995929122@qq.com>
2025-10-13 11:08:23 +0800

    20-有效的括号

---
+++ LeetCode/25年10月/10.13/isValid.go
@@ -0,0 +1,30 @@
+package temp
+
+func isValid(s string) bool {
+	n := len(s)
+	stack := make([]rune, 0, n)
+
+	for _, v := range s {
+		switch v {
+		case '(', '[', '{':
+			stack = append(stack, v)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
