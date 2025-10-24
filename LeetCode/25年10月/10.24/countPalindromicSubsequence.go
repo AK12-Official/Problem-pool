ea1fb28dd1c9e383f48f27db2db588754accce8a
Ak12_Official <995929122@qq.com>
2025-10-24 19:07:25 +0800

    1930-长度为3的不同回文自序列-0x3f

--- LeetCode/25年10月/10.24/countPalindromicSubsequence.go
+++ LeetCode/25年10月/10.24/countPalindromicSubsequence.go
@@ -1,5 +1,7 @@
 package temp
 
+import "math/bits"
+
 func countPalindromicSubsequence(s string) (ans int) {
 
 	suf := make(map[rune]int)
@@ -24,5 +26,26 @@ func countPalindromicSubsequence(s string) (ans int) {
 		pre[x]++
 	}
 
+	return
+}
+
+func countPalindromicSubsequencePRO(s string) (ans int) {
+	var pre, suf, has [26]int
+	for _, ch := range s[1:] {
+		suf[ch-'a']++
+	}
+	for i := 1; i < len(s)-1; i++ { // 枚举回文子序列的中间字符
+		pre[s[i-1]-'a']++
+		suf[s[i]-'a']--
+		for j := 0; j < 26; j++ { // 枚举中间字符的左右字符
+			if pre[j] > 0 && suf[j] > 0 { // 找到了一个回文子序列
+				has[s[i]-'a'] |= 1 << j
+				//天才！ 利用int的32位以位图的形式存储回文串的中间字符 天才！nb
+			}
+		}
+	}
+	for _, mask := range has {
+		ans += bits.OnesCount(uint(mask)) // 累加二进制中 1 的个数即为答案
+	}
 	return
 }
